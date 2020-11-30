0809b8894e269fa7dfeac375d3121a08ba246922
Luca Coduri <coduri.vitor@gmail.com>
2020-11-30 23:38:53 +0100

    petit truc

--- jeuDeLaVieCalculs.cpp
+++ jeuDeLaVieCalculs.cpp
@@ -28,7 +28,7 @@ bool etatCelluleGenSuivante(bool etatActuel, unsigned nbOccurences) {
 
 Grille calculeGenSuivante(const Grille &genActu) {
    assert(verifierTailleGrille(genActu.size(), genActu.at(0).size()));
-   Grille genSuiv(genActu.size(),Ligne(genActu.at(0).size()));
+   Grille genSuiv(genActu.size(), Ligne(genActu.at(0).size()));
 
    for (size_t i = 0; i < genActu.size(); i++) {
       for (size_t j = 0; j < genActu.at(i).size(); j++) {
@@ -47,18 +47,18 @@ Grille calculeGenSuivante(const Grille &genActu) {
 unsigned nbCellulesVoisinesOccupees(const Grille &grille, size_t ligne, size_t
 colonne) {
    assert(verifierTailleGrille(grille.size(), grille.at(0).size()));
-   int occ;
    size_t ligneFin = grille.size() - 1;
    size_t colonneFin = grille.at(0).size() - 1;
 
    // Calcule le nombre d'occurences dans le cas où la cellule se trouve dans un
    // des bords de la grille puis (dans le else) dans le cas où la cellule se
    // trouve dans le centre de la grille
+   int occ;
    if (ligne == 0) {
       if (colonne == 0) {
          occ = grille.at(0).at(1)
-            + grille.at(1).at(0)
-            + grille.at(1).at(1);
+               + grille.at(1).at(0)
+               + grille.at(1).at(1);
       } else if (colonne == colonneFin) {
          occ = grille.at(0).at(colonneFin - 1)
                + grille.at(1).at(colonneFin - 1)
@@ -73,17 +73,17 @@ colonne) {
    } else if (ligne == ligneFin) {
       if (colonne == 0) {
          occ = grille.at(ligneFin - 1).at(0) + grille.at(ligneFin - 1).at(1)
-                + grille.at(ligneFin).at(1);
+               + grille.at(ligneFin).at(1);
       } else if (colonne == colonneFin) {
          occ = grille.at(ligneFin - 1).at(colonneFin - 1)
-                + grille.at(ligneFin - 1).at(colonneFin)
-                + grille.at(ligneFin).at(colonneFin - 1);
+               + grille.at(ligneFin - 1).at(colonneFin)
+               + grille.at(ligneFin).at(colonneFin - 1);
       } else {
          occ = grille.at(ligneFin - 1).at(colonne - 1)
-                + grille.at(ligneFin - 1).at(colonne)
-                + grille.at(ligneFin - 1).at(colonne + 1)
-                + grille.at(ligneFin).at(colonne - 1)
-                + grille.at(ligneFin).at(colonne + 1);
+               + grille.at(ligneFin - 1).at(colonne)
+               + grille.at(ligneFin - 1).at(colonne + 1)
+               + grille.at(ligneFin).at(colonne - 1)
+               + grille.at(ligneFin).at(colonne + 1);
       }
    } else {
       if (colonne == 0) {
@@ -109,5 +109,5 @@ colonne) {
                + grille.at(ligne - 1).at(colonne + 1);
       }
    }
-   return (unsigned)occ;
+   return (unsigned) occ;
 }
